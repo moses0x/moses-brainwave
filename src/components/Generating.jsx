97ce10d4df21f0c1991ae0441bf12a83eeb0605b
Moses <mosesajila@gmail.com>
2024-05-14 23:09:38 +0100

    first commit

---
+++ src/components/Generating.jsx
@@ -0,0 +1,15 @@
+import { loading } from "../assets";
+
+const Generating = ({ className }) => {
+  return (
+    <div
+      className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${
+        className || ""
+      } text-base`}
+    >
+      AI is generating
+    </div>
+  );
+};
+
+export default Generating;
